89f8515344bc735f50fc24be8000453698477ec0
Otoloye Oyeniran <otoloyeoyeniran@gmail.com>
2019-11-28 03:43:44 -0500

    show response url

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import './App.css';
 const initialState = ['Area boys', 'Boys II Men', 'Fela Kuti'];
 
 function App() {
-  const [books, setBooks] = useState([initialState]);
+  const [books, setBooks] = useState([]);
+
+  let booksCopy = books;
 
   useEffect(() => {
     fetch(
       'https://openlibrary.org/api/books?bibkeys=ISBN:0385472579,LCCN:62019420&format=json'
     ).then(res => {
-      console.log('show books from ISBN:', res);
+      console.log('show books from ISBN:', res.url);
+      setBooks([...booksCopy, res.url]);
     });
   }, []);
 
   return (
     <div className="app-root">
       <h1>This is a list of the books</h1>
-      {/* <div>
+      <div>
         {books.map(book => (
           <p>{book}</p>
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
