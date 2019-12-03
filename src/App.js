bd037ed8f6fd0eae2d8ced7dedaedfb8631f81b3
Otoloye Oyeniran <otoloyeoyeniran@gmail.com>
2019-12-02 20:54:12 -0500

    Card to display books

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-
-const initialState = ['Area boys', 'Boys II Men', 'Fela Kuti'];
+import ImgMediaCard from './card';
+import CustomizedInputBase from './search';
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  let booksCopy = books;
-
-  useEffect(() => {
-    fetch(
-      'https://openlibrary.org/api/books?bibkeys=ISBN:0385472579,LCCN:62019420&format=json'
-    ).then(res => {
-      console.log('show books from ISBN:', res.url);
-      setBooks([...booksCopy, res.url]);
-    });
-  }, []);
-
   return (
-    <div className="app-root">
-      <h1>This is a list of the books</h1>
-      <div>
-        {books.map(book => (
-          <p>{book}</p>
-        ))}
+    <div>
+      <CustomizedInputBase />
+      <div className="cards-container">
+        <ImgMediaCard />
+        <ImgMediaCard />
+        <ImgMediaCard />
+        <ImgMediaCard />
       </div>
     </div>
   );
